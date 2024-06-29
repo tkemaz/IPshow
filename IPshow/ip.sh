a2849d961fea5a9c62e89cce524bfc3343f74ede
tkemaz <174154109+tkemaz@users.noreply.github.com>
2024-06-29 17:42:13 +0200

    ip.sh

--- IPshow/ip.sh
+++ IPshow/ip.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-get_ip_address() {
-  ip_address=$(ip addr show | awk '/inet / {print $2}' | cut -d/ -f1 | head -1)
-  if [ -z "$ip_address" ]; then
-    ip_address="127.0.0.1"
-  fi
-  echo "$ip_address"
-}
-
-echo "ip: $(get_ip_address)" # Print ip address to the terminal
+chmod +x ip.sh
+python3 ./ip.py
